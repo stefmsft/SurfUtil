2f2aa9f6b1494ba20e64a4452df91caa9e25b757
stephsau <stephsau@microsoft.com>
2019-01-31 09:32:12 +0000

    Reset content before Upload of Build 20190131-01 to GitHub

--- setup-surfUtil.cmd
+++
@@ -1,5 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco upgrade chocolatey
-choco install git -y -x
-mkdir SurfUtil
-"C:\Program Files\Git\cmd\git.exe" clone https://github.com/stefmsft/SurfUtil.git .\SurfUtil\
